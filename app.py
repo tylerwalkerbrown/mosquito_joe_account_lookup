5e3352e52c2f25bf4f7f9b1906f308b0326c116d
Tyler Brown <94020684+tylerwalkerbrown@users.noreply.github.com>
2023-11-24 19:58:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,19 @@ def streamlit_main():
     st.text("Below are the requirements for this Streamlit app:")
     st.code(requirements, language='bash')
 
-def load_data(uploaded_file):
-    return pd.read_csv(uploaded_file)
+import streamlit as st
+import pandas as pd
 
-def perform_merge(df1, df2, df3):
+def load_and_merge_data(file1, file2, file3, account_number):
+    df1 = pd.read_csv(file1)
+    df2 = pd.read_csv(file2)
+    df3 = pd.read_csv(file3)
+    
     merge1 = pd.merge(df1, df2, on='accountnum')
     merge2 = pd.merge(merge1, df3, on='accountnum')
-    return merge2[['Businessname', 'completeddate', 'accountnum', 'SumOfbillamount', 'saledate', 'duration', 'measurement']]
+    merge2 = merge2[merge2['accountnum'] == account_number]
+    
+    return merge2
 
 def calculate_aggregates(df):
     total_bill = pd.DataFrame(df.groupby(['Businessname', 'accountnum'])['SumOfbillamount'].sum()).reset_index()
@@ -25,47 +31,27 @@ def calculate_aggregates(df):
     merge3 = pd.merge(count, total_bill, on='accountnum')
     merge3.rename(columns={'completeddate': 'Total_Completed', 'SumOfbillamount': 'Total_Bill'}, inplace=True)
     merge3['avg_bill'] = merge3['Total_Bill'] / merge3['Total_Completed']
+    
     return merge3
 
-# Streamlit app
 def streamlit_main():
-    st.title("CSV Merger, Aggregator, and Search")
+    st.title("Data Aggregation and Display")
 
-    # File uploaders
-    st.write("Completed WO Detail By Date - ")
-    csv_1 = st.file_uploader("Choose the first CSV file", type=["csv"])
-    st.write("price History of Events")
-    csv_2 = st.file_uploader("Choose the second CSV file", type=["csv"])
-    st.write("Account Detail by Event By Sort")
-    csv_3 = st.file_uploader("Choose the third CSV file", type=["csv"])
+    account_number = st.number_input("Enter an account number", value=1002406, step=1)
 
-    if csv_1 and csv_2 and csv_3:
-        df1 = load_data(csv_1)
-        df2 = load_data(csv_2)
-        df3 = load_data(csv_3)
+    uploaded_file_1 = st.file_uploader("Upload first CSV file", type="csv", key="file1")
+    uploaded_file_2 = st.file_uploader("Upload second CSV file", type="csv", key="file2")
+    uploaded_file_3 = st.file_uploader("Upload third CSV file", type="csv", key="file3")
 
-        # Merge and calculate aggregates
-        merged_df = perform_merge(df1, df2, df3)
-        aggregates_df = calculate_aggregates(merged_df)
+    if uploaded_file_1 and uploaded_file_2 and uploaded_file_3:
+        merged_df = load_and_merge_data(uploaded_file_1, uploaded_file_2, uploaded_file_3, account_number)
+        df = merged_df[['Businessname', 'completeddate', 'accountnum', 'SumOfbillamount', 'saledate', 'duration', 'measurement']]
+        aggregates_df = calculate_aggregates(df)
 
-        # Search box to filter results
-        account_search = st.text_input("Enter an account number to search:")
-
-        if account_search:
-            # Filter both DataFrames based on the search_value
-            filtered_merged_df = merged_df[merged_df['accountnum'] == account_search]
-            filtered_aggregates_df = aggregates_df[aggregates_df['accountnum'] == account_search]
-
-            # Display the filtered merged DataFrame
-            st.write("Filtered Merged Data")
-            st.dataframe(filtered_merged_df)
-
-            # Display the filtered aggregates DataFrame
-            st.write("Filtered Aggregated Data")
-            st.dataframe(filtered_aggregates_df)
-        else:
-            # Display instructions
-            st.write("Enter an account number to filter the data.")
+        st.subheader("Aggregated Data")
+        st.dataframe(aggregates_df)
+    else:
+        st.write("Please upload all three CSV files to proceed.")
 
 if __name__ == "__main__":
     streamlit_main()
